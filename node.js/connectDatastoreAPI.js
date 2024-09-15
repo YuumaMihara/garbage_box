c762a5455e90ff7722b65123e9421d2ac9ee4e5e
YuumaMihara <mihara.y87@gmail.com>
2024-09-15 21:04:13 +0900

    node.jsでのhttpサーバー機能を追加
    詳細画面表示時、node.jsで起動したhttpサーバーにhttpでアクセスし、GETパラメタに応じたdatastore内の値を受け取る処理を実装
    その際にcorsエラーを回避するための処理を追加

---
+++ node.js/connectDatastoreAPI.js
@@ -0,0 +1,29 @@
+﻿//cloud datastoreのクライアントをインポート
+const { Datastore, PropertyFilter } = require('@google-cloud/datastore');
+
+// Datastore クライアントを作成
+const datastore = new Datastore({
+  projectId: 'web-photo-gallery-429509',
+  keyFilename: '../key/cloud_strage_api_key.json'
+});
+
+// エンティティを取得するための関数
+// name: datastore内のキー名
+async function getEntity(name) {
+  // テーブル(Kind)が'Contents'かつ、キーが一致するエンティティを検索
+  const query = datastore
+    .createQuery('Contents')
+    .filter(
+      new PropertyFilter("__key__", "=", datastore.key(["Contents", name]))
+    );
+
+  try {
+    const entity = await datastore.runQuery(query);
+    return entity;
+    // console.log(JSON.stringify(entity));
+  } catch (err) {
+    console.error('エンティティの取得中にエラーが発生しました:', err);
+  }
+}
+
+exports.getEntity = getEntity;
---
+++ node.js/server.js
@@ -0,0 +1,33 @@
+﻿// httpサーバー起動用
+const http = require('http');
+// リクエスト内のGETパラメタ解析用
+const url = require('url');
+// datastore接続用
+const datastore = require('./connectDatastoreAPI');
+
+async function callAPI(req, res) {
+    const param = url.parse(req.url, true).query;
+    const entity = await datastore.getEntity(param.name);
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost");
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(entity));
+    res.end();
+}
+
+function notFound(req, res) {
+    res.writeHead(404);
+    res.end('Not Found');
+}
+
+const server = http.createServer((req, res) => {
+    if (!req.url.indexOf('/getEntity')) {
+        callAPI(req, res);
+    } else if(!req.url.indexOf('/about')) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(req.url);
+    } else {
+        notFound(req, res);
+    }
+});
+
+server.listen(3000);
