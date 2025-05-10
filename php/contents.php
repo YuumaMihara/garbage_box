a2a655f318d87308a08b2870b74df1f7165447c3
YuumaMihara <mihara.y87@gmail.com>
2025-05-10 18:33:09 +0900

    本番環境用にurl変更

--- php/contents.php
+++ php/contents.php
@@ -23,9 +23,9 @@
                 <span class="lines line-3"></span>
             </label>
             <ul class="menu_box">
-                <li><a class="menu_item" href="index.php">TOP</a></li>
-                <li><a class="menu_item" href="contents.php">CONTENTS</a></li>
-                <li><a class="menu_item" href="about.php">ABOUT</a></li>
+                <li><a class="menu_item" href="/">TOP</a></li>
+                <li><a class="menu_item" href="php/contents.php">CONTENTS</a></li>
+                <li><a class="menu_item" href="php/about.php">ABOUT</a></li>
             </ul>
         </nav>
     </header>
